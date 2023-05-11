6fed1f3eb8345eb88a07d1440a7db6ae2f6218b4
Racksoup <rack.connor1@gmail.com>
2023-05-11 07:31:07 -0400

    spider animating

--- src/components/ThreeScene.jsx
+++ src/components/ThreeScene.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
 // import Donut from '../assets/models/Donut8.glb';
-import Donut from '../assets/models/Spider1.glb';
+import Donut from '../assets/models/Spider66.glb';
 
 import { Canvas, useFrame } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
@@ -15,27 +15,28 @@ export default function ThreeScene() {
   useEffect(() => {
     const gltfLoader = new GLTFLoader();
     gltfLoader.load(Donut, (gltf) => {
-      gltf.scene.scale.set(7, 7, 7);
+      gltf.scene.scale.set(5, 5, 5);
       gltf.scene.rotateY(3.14);
       gltf.scene.rotateX(-0.45);
       gltf.scene.position.y = -2;
 
-      // mixer.current = new THREE.AnimationMixer(gltf.scene);
-      // const animationAction = mixer.current.clipAction(gltf.animations[0]);
-      // animationAction.play();
+      mixer.current = new THREE.AnimationMixer(gltf.scene);
+      const animationAction = mixer.current.clipAction(gltf.animations[0]);
+      animationAction.play();
 
+      setDonut(gltf.scene);
       const root = gltf.scene;
       group.current.add(root);
-      setDonut(gltf.scene);
     });
   }, []);
 
   return (
     <div className='ThreeScene'>
       <Canvas style={{ backgroundColor: 'rgba(21,21,21,1)' }}>
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-        <pointLight position={[-10, -10, -10]} />
+        {/* <ambientLight intensity={0.12} /> */}
+        <spotLight position={[10, 20, 10]} angle={0.15} penumbra={1} />
+        {/* <spotLight position={[-200, -200, -200]} angle={0.15} penumbra={0.1} intensity={0.2} /> */}
+        <pointLight position={[-50, -10, -100]} />
         <group ref={group}>
           <mesh />
           {donut && <AnimatedDonut donut={donut} mixer={mixer} clock={clock} />}
